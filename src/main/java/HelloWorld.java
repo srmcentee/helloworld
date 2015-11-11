c44783c360d7b2c98f2373515c8e264d28c7b4f2
Scott McEntee <scott.mcentee@ca.com>
2015-11-11 13:57:48 -0800

    Push initial java code

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ * Simple Hellow World app
+ * 
+ */
+public class HelloWorld {
+     
+     
+    static final String HelloWorld = "Hello World"; 
+ 
+    public static void main(String[] args) {
+                     
+          
+        try {
+            System.out.println("Hello World ");  
+        }
+        catch (Exception e) {
+            System.out.println("Failure to communicate!");    
+        }
+    }
+}
